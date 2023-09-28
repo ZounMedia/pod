4a60961ec81451633217da1862516e844ae9f0b5
Romaric Zounlome <rzounlom@gmail.com>
2023-09-28 09:43:32 -0400

    complete form validation functionality

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
-//store button, input, and error span
+//store button, input, and error span as variables
 let input = document.querySelector("input");
 const submitBtn = document.querySelector("button");
 let errorText = document.querySelector("span.error");
 
 //add click event listener to button
-submitBtn.addEventListener("click", () => {
-  console.log({ input, submitBtn, errorText });
+submitBtn.addEventListener("click", (event) => {
+  //   console.log({ input, submitBtn, errorText, inputValue: input.value });
+
   //reset error text value to empty
   errorText.innerHTML = "";
-
-  console.log("input value", input.value);
 
   if (
     input.value.length === 0 ||
     !(input.value.includes("@") && input.value.includes(".com"))
   ) {
     errorText.innerHTML = "Oops! Please check your email";
+    //prevent form from submitting
+    event.preventDefault();
     return;
   }
 
